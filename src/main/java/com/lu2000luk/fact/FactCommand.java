9392caadb80124a44108c1f13909f771d37e1115
lu2000luk <lu2000luk@gmail.com>
2025-02-04 19:06:18 +0100

    Command

--- src/main/java/com/lu2000luk/fact/FactCommand.java
+++ src/main/java/com/lu2000luk/fact/FactCommand.java
@@ -19,14 +19,58 @@ import java.util.List;
 
 public class FactCommand {
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
-        dispatcher.register(Commands.literal("fact").then(Commands.argument("action", StringArgumentType.word())
-                .executes(commandContext -> execute(commandContext, StringArgumentType.getString(commandContext, "action")))));
+        dispatcher.register(Commands.literal("fact")
+                .requires(cs -> cs.hasPermission(0))
+                .then(
+                        Commands.literal("admin")
+                                .requires(cs -> cs.hasPermission(4))
+                                .then(Commands.literal("create")
+                                        .then(Commands.argument("name", StringArgumentType.string())
+                                                .executes(ctx -> createTeam(ctx, StringArgumentType.getString(ctx, "name")))
+                                        )
+                                )
+                                .then(Commands.literal("delete")
+                                        .then(Commands.argument("name", StringArgumentType.string())
+                                                .executes(ctx -> deleteTeam(ctx, StringArgumentType.getString(ctx, "name")))
+                                        )
+                                )
+                                .then(Commands.literal("claim").then(Commands.argument("as", StringArgumentType.string())
+                                        .executes(ctx -> claimChunk(ctx, StringArgumentType.getString(ctx, "as")))
+                                ))
+                                .then(Commands.literal("unclaim").then(Commands.argument("as", StringArgumentType.string())
+                                        .executes(ctx -> unclaimChunk(ctx, StringArgumentType.getString(ctx, "as")))
+                                ))
+                ));
     }
 
     private static int execute(CommandContext<CommandSourceStack> command, String arg) {
         if (command.getSource().getEntity() instanceof Player player) {
             player.sendSystemMessage(Component.literal("Fact >> Hello, " + player.getStringUUID()));
         }
+        return Command.SINGLE_SUCCESS;
+    }
+
+    private static int createTeam(CommandContext<CommandSourceStack> command, String name) {
+
+
+        return Command.SINGLE_SUCCESS;
+    }
+
+    private static int deleteTeam(CommandContext<CommandSourceStack> command, String name) {
+
+
+        return Command.SINGLE_SUCCESS;
+    }
+
+    private static int claimChunk(CommandContext<CommandSourceStack> command, String name) {
+
+
+        return Command.SINGLE_SUCCESS;
+    }
+
+    private static int unclaimChunk(CommandContext<CommandSourceStack> command, String name) {
+
+
         return Command.SINGLE_SUCCESS;
     }
 }
